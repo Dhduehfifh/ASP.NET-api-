494c6732af0b04147b9775dd196eb3e81fde6dd8
Dhduehfifh <starkli557@gmail.com>
2025-03-17 14:04:04 -0400

    这个数据库配置一部分暂时留空，因为不知道具体后面怎么写

--- Core/Database/DBConfig.cs
+++ Core/Database/DBConfig.cs
@@ -4,6 +4,20 @@ namespace Database
 {
     public class DBConfig
     {
+        public string DatabaseType { get; set; }
+        public string ConnectionString { get; set; }
         
+        // 给后面留着orm映射
+        public Dictionary<string, string> TableMappings { get; set; }
+
+        public string ParamStyle { get; set; } = "@";
+        public bool Transactional { get; set; } = true;
+
+        //获取适配器（暂时为空，后期再尝试实现）
+        public ISqlAdapter GetSqlAdapter()
+        {
+            //按照实际情况返回适配器，暂时留空
+            throw new NotImplementedException();
+        }
     }
 }
