5892e403317ef5c5201fdb8defc14e31b76a8af3
Dhduehfifh <starkli557@gmail.com>
2025-03-18 00:29:26 -0400

    更新了外部网关，但是引用好像出了问题

--- obj/Debug/net9.0/demo1.AssemblyInfo.cs
+++ obj/Debug/net9.0/demo1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("demo1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbc14d52170634541a2cc89f1f5f0790a385b37a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c9eff78aafc80aaa8a8f94909ca340fb08c50f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("demo1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("demo1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
